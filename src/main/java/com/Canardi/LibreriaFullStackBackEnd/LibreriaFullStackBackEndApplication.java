c63720ecc6980783a3607d15f61b82faf46b1b84
PATRICIORICARDI <patricio.ricardi@gmail.com>
2024-11-04 21:35:06 -0600

    FIRST COMMIT
    Se crea proyecto con ayuda de spring initializr y se instalan dependencias.

---
+++ src/main/java/com/Canardi/LibreriaFullStackBackEnd/LibreriaFullStackBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.Canardi.LibreriaFullStackBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaFullStackBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaFullStackBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Canardi/LibreriaFullStackBackEnd/LibreriaFullStackBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Canardi.LibreriaFullStackBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaFullStackBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
